283a802fea7094929a3c4bed3d082f83ec099b12
Melissa <meli8538@gmail.com>
2020-02-03 11:51:53 -0600

    Add RequestModel

---
+++ Vacations/Models/RequestModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Vacations.Models
+{
+    public class RequestModel
+    {
+
+        public class Request
+        {
+            public int requestId { get; set; }
+
+            [Required(ErrorMessage = "State must be specified")]
+            public string state { get; set; }
+
+            [Required(ErrorMessage = "Description must be specified")]
+            public string description { get; set; }
+
+
+            public int daysRequestedCount { get; set; }
+            public int midDaysCount { get; set; }
+            public int PersonpersonaId { get; set; }
+            public System.DateTime createdAt { get; set; }
+            public System.DateTime updatedAt { get; set; }
+            public int createdBy { get; set; }
+            public int updatedBy { get; set; }
+
+        }
+    }
+}
